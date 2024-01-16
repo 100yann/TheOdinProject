a5976f8c8fe4ea4c8fef64f2aead017b6f89ef66
100yann <stoyan.d.kolev@gmail.com>
2024-01-16 13:44:52 +0200

    disable submit button if email input field is empty

--- medito_website/index.js
+++ medito_website/index.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
     displayProgressTitle()
     loadProgressBar()
 
+    // Reveal answer on click of question in FAQ
     const questionDropdowns = document.querySelectorAll('#question')
     questionDropdowns.forEach((question) => {
         question.addEventListener('click', () => {dropdownAnswer(question)}) 
     })
+
+    const emailInput = document.getElementById('email-input')
+    emailInput.addEventListener('keyup', () => {validateEmailField(emailInput)})
 })
 
 // 
@@ -25,8 +29,18 @@ function loadProgressBar() {
     barProgress.style.animationName = 'loadbar'
 }
 
+
 function dropdownAnswer(question) {
     const answer = question.parentElement.children[1]
-    console.log(answer)
     answer.hidden ? answer.hidden = false : answer.hidden = true;
+}
+
+// Check if email field is filled
+function validateEmailField(emailInput) {
+    const emailSubmit = document.getElementById('email-submit')
+    if (!emailInput.value) {
+        emailSubmit.disabled = true;
+    } else {
+        emailSubmit.disabled = false;
+    };
 }
